dbbc8d192011248d3892520b67e27d8cb31af13c
tranhuuhau2003 <96118209+tranhuuhau2003@users.noreply.github.com>
2023-05-12 16:34:55 +0700

    heheheheehe

--- lagi_shop.sql
+++ lagi_shop.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 10, 2023 lúc 09:05 AM
--- Phiên bản máy phục vụ: 10.4.25-MariaDB
--- Phiên bản PHP: 8.1.10
+-- Thời gian đã tạo: Th5 12, 2023 lúc 11:34 AM
+-- Phiên bản máy phục vụ: 10.4.27-MariaDB
+-- Phiên bản PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,7 +34,7 @@ CREATE TABLE `admin` (
   `adminUser` varchar(255) NOT NULL,
   `adminPass` varchar(255) NOT NULL,
   `level` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `admin`
@@ -53,7 +53,7 @@ INSERT INTO `admin` (`adminId`, `adminName`, `adminEmail`, `adminUser`, `adminPa
 CREATE TABLE `brand` (
   `brandId` int(11) NOT NULL,
   `brandName` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `brand`
@@ -84,7 +84,7 @@ CREATE TABLE `cart` (
   `productId` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `quantity` int(11) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `cart`
@@ -102,7 +102,7 @@ INSERT INTO `cart` (`cartID`, `productId`, `userId`, `quantity`) VALUES
 CREATE TABLE `category` (
   `catId` int(11) NOT NULL,
   `catName` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `category`
@@ -134,7 +134,7 @@ CREATE TABLE `comment` (
   `comment` varchar(255) NOT NULL,
   `productId` int(11) NOT NULL,
   `dateComment` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -152,7 +152,7 @@ CREATE TABLE `order` (
   `recipientName` varchar(255) NOT NULL,
   `recipientPhone` int(11) NOT NULL,
   `recipientAddress` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `order`
@@ -176,7 +176,7 @@ CREATE TABLE `order_detail` (
   `image` varchar(255) NOT NULL,
   `quantity` int(11) NOT NULL,
   `productName` tinytext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `order_detail`
@@ -206,7 +206,7 @@ CREATE TABLE `product` (
   `amount` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `hidden` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `product`
@@ -228,14 +228,14 @@ INSERT INTO `product` (`productId`, `productName`, `catId`, `brandId`, `product_
 
 CREATE TABLE `user` (
   `userId` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `userPassword` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `gioiTinh` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `sdt` int(11) NOT NULL,
-  `ngaySinh` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `diaChi` text COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `userPassword` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gioiTinh` varchar(255) NOT NULL,
+  `sdt` varchar(20) NOT NULL,
+  `ngaySinh` varchar(255) NOT NULL,
+  `diaChi` varchar(200) NOT NULL,
   `isActive` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -244,23 +244,26 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`userId`, `name`, `username`, `userPassword`, `email`, `gioiTinh`, `sdt`, `ngaySinh`, `diaChi`, `isActive`) VALUES
-(7, 'Quan Văn Mạnh', 'manhne', '202cb962ac59075b964b07152d234b70', 'quanmanh901@gmail.com', 'nam', 899391826, '18-04-2002', '309/1/4 Lê Đức Thọ', 0),
-(8, 'Quan Văn Mạnh', 'manh22', '202cb962ac59075b964b07152d234b70', 'manh2106@gmail.com', 'Nam', 981984623, '2002-21-06', 'Hồ Chí Minh', 0),
-(10, 'Phùng Phạm Khánh Linh', 'khanhlinh', '202cb962ac59075b964b07152d234b70', 'nguyenthanhquynhlinh@gmail.com', 'Nam', 981984623, '2002-21-06', 'Hồ Chí Minh', 1),
-(15, 'Trần Thẩm Khang', 'admin1@gmail.com', '$2y$10$qd/KrSsHtR9pXT6AJCei8O7ZiRNYhXkmdS1kUq90eE1fS1doo0bve', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-04-18', 'Tỉnh Cao Bằng, Huyện Bảo Lâm, Xã Nam Cao, khang', 1),
-(17, 'Trần Thẩm Khang', 'thamkhang', '$2y$10$zuf/Ax5rLhvrPR0yfIDEKeDVP0sQWnxOgntm6YvLWFZc7WwVJMny.', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-04-11', 'Thành phố Hà Nội, Quận Long Biên, Phường Thượng Thanh', 1),
-(18, 'Trần Thẩm Khang', 'admin', '$2y$10$8cnwaC4kyEH/pcfi241ED.qlpqSo6ejyO46N8ps.EVu5mt2t.XY0u', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-05-10', 'Tỉnh Cao Bằng, Huyện Bảo Lâm, Xã Nam Cao, xas', 1),
-(19, 'Trần Thẩm Khang', 'admin@gmail.com', '$2y$10$ikQFWcJfx6aIRclINa2Cke9SymuUDb4SHc0Ou9Dq74oEyZYe/f2Ya', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-05-01', 'Thành phố Hà Nội, Quận Ba Đình, Phường Phúc Xá, khang', 0),
-(24, 'Trần Thẩm Khang', 'trankhang', '$2y$10$8EmAYzBr9NSHA0lxlFsbR.hco2dipxa8f.evJ5f.NbYFvRkA40JXa', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-05-02', 'Thành phố Hà Nội, Quận Ba Đình, Phường Trúc Bạch, Thành phố Hà Nội, Quận Ba Đình, Phường Trúc Bạch, warwetat4w', 0),
-(27, 'Trần Thẩm Khang', 'khang1', '$2y$10$HrAzqBMvGvXlCrcx5JoryeGeLmgijAxbi03qA/iSmFUrIagIZSUo.', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-05-10', 'Tỉnh Cao Bằng, Huyện Bảo Lạc, Xã Cô Ba, Tỉnh Cao Bằng, Huyện Bảo Lạc, Xã Cô Ba, dfsagafg', 0),
-(28, 'Trần Thẩm Khang', 'khang123', '$2y$10$rvtZLkND3zKrLUXMrVPFTeisEedr6GrsUujfIRMymK28HuYN5rJU2', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-05-02', 'Tỉnh Hà Giang, Thành phố Hà Giang, Phường Quang Trung, sfsadf', 0),
-(29, 'Trần Thẩm Khang', 'khang12', '$2y$10$QvWiKsdrpsTbOTHCudfSX.aFrQDy7ckmWb3pfwxFRUSa2fD0kWwi6', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-05-02', 'Thành phố Hà Nội, Quận Hoàn Kiếm, Phường Đồng Xuân, Tỉnh Hà Giang, Thành phố Hà Giang, Phường Quang Trung, sfsadf', 0),
-(30, 'Trần Thẩm Khang', 'linh', '$2y$10$929yIWVV3g1pWompiZqli.AzFjELQsRU9.ZWFX6r1lMwb2U/ceZgK', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-05-02', 'Tỉnh Lào Cai, Huyện Bắc Hà, Xã Tả Van Chư, asdfsadf', 0),
-(31, 'Trần Thẩm Khang', 'khang12345', '$2y$10$P9/uQnoG/x1DOTKXU/ULCeDAZH6/NrYA39LTQnSLmHFMsEf/JVjkm', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-05-01', 'Thành phố Hà Nội, Quận Hoàng Mai, Phường Hoàng Văn Thụ, asdfasdf', 0),
-(32, 'Trần Thẩm Khang', 'admin123', '$2y$10$whqz9WbpkYb79VkXw0r9S.m.E7t2amoXCCXhWkXx2tUPyk/2fGz2y', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-05-03', 'Tỉnh Tuyên Quang, Huyện Na Hang, Xã Thượng Giáp, sdafasdf', 0),
-(33, 'Trần Thẩm Khangasdf', 'wefwaf', '$2y$10$A7nA92T3MNVl78JJRHJSeOqdXhb3t0WKRSS1g3MxS0CYPsJsQY9.K', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-05-02', 'Tỉnh Quảng Ninh, Huyện Ba Chẽ, Xã Thanh Lâm, asdfdsag', 0),
-(34, 'Trần Thẩm Khang', 'dfa', '$2y$10$Ug7PkdDk3w1qdLHwiLeRg.388Wi5ks0LIkEF0lxJb2GHgbQyH626u', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-05-03', 'Tỉnh Quảng Ninh, Huyện Tiên Yên, Xã Đại Dực, asdfasdf', 0),
-(37, 'Trần Thẩm Khang', 'admin@gmail.comasdf', '$2y$10$vTTbIMMwle5ubALiVM/y8eo8I6WchxIz.D8JR8GQwlCoCC5HaFueS', 'thamkhang2003@gmail.com', 'Nam', 2147483647, '2023-05-18', 'sdafs', 0);
+(7, 'Quan Văn Mạnh', 'manhne', '202cb962ac59075b964b07152d234b70', 'quanmanh901@gmail.com', 'nam', '899391826', '18-04-2002', '309/1/4 Lê Đức Thọ', 0),
+(8, 'Quan Văn Mạnh', 'manh22', '202cb962ac59075b964b07152d234b70', 'manh2106@gmail.com', 'Nam', '981984623', '2002-21-06', 'Hồ Chí Minh', 0),
+(10, 'Phùng Phạm Khánh Linh', 'khanhlinh', '202cb962ac59075b964b07152d234b70', 'nguyenthanhquynhlinh@gmail.com', 'Nam', '981984623', '2002-21-06', 'Hồ Chí Minh', 1),
+(15, 'Trần Thẩm Khang', 'admin1@gmail.com', '$2y$10$qd/KrSsHtR9pXT6AJCei8O7ZiRNYhXkmdS1kUq90eE1fS1doo0bve', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-04-18', 'Tỉnh Cao Bằng, Huyện Bảo Lâm, Xã Nam Cao, khang', 1),
+(17, 'Trần Thẩm Khang', 'thamkhang', '$2y$10$zuf/Ax5rLhvrPR0yfIDEKeDVP0sQWnxOgntm6YvLWFZc7WwVJMny.', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-04-11', 'Thành phố Hà Nội, Quận Long Biên, Phường Thượng Thanh', 1),
+(18, 'Trần Thẩm Khang', 'admin', '$2y$10$8cnwaC4kyEH/pcfi241ED.qlpqSo6ejyO46N8ps.EVu5mt2t.XY0u', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-05-10', 'Tỉnh Cao Bằng, Huyện Bảo Lâm, Xã Nam Cao, xas', 1),
+(19, 'Trần Thẩm Khang', 'admin@gmail.com', '$2y$10$ikQFWcJfx6aIRclINa2Cke9SymuUDb4SHc0Ou9Dq74oEyZYe/f2Ya', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-05-01', 'Thành phố Hà Nội, Quận Ba Đình, Phường Phúc Xá, khang', 0),
+(24, 'Trần Thẩm Khang', 'trankhang', '$2y$10$8EmAYzBr9NSHA0lxlFsbR.hco2dipxa8f.evJ5f.NbYFvRkA40JXa', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-05-02', 'Thành phố Hà Nội, Quận Ba Đình, Phường Trúc Bạch, Thành phố Hà Nội, Quận Ba Đình, Phường Trúc Bạch, warwetat4w', 0),
+(27, 'Trần Thẩm Khang', 'khang1', '$2y$10$HrAzqBMvGvXlCrcx5JoryeGeLmgijAxbi03qA/iSmFUrIagIZSUo.', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-05-10', 'Tỉnh Cao Bằng, Huyện Bảo Lạc, Xã Cô Ba, Tỉnh Cao Bằng, Huyện Bảo Lạc, Xã Cô Ba, dfsagafg', 0),
+(28, 'Trần Thẩm Khang', 'khang123', '$2y$10$rvtZLkND3zKrLUXMrVPFTeisEedr6GrsUujfIRMymK28HuYN5rJU2', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-05-02', 'Tỉnh Hà Giang, Thành phố Hà Giang, Phường Quang Trung, sfsadf', 0),
+(29, 'Trần Thẩm Khang', 'khang12', '$2y$10$QvWiKsdrpsTbOTHCudfSX.aFrQDy7ckmWb3pfwxFRUSa2fD0kWwi6', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-05-02', 'Thành phố Hà Nội, Quận Hoàn Kiếm, Phường Đồng Xuân, Tỉnh Hà Giang, Thành phố Hà Giang, Phường Quang Trung, sfsadf', 0),
+(30, 'Trần Thẩm Khang', 'linh', '$2y$10$929yIWVV3g1pWompiZqli.AzFjELQsRU9.ZWFX6r1lMwb2U/ceZgK', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-05-02', 'Tỉnh Lào Cai, Huyện Bắc Hà, Xã Tả Van Chư, asdfsadf', 0),
+(31, 'Trần Thẩm Khang', 'khang12345', '$2y$10$P9/uQnoG/x1DOTKXU/ULCeDAZH6/NrYA39LTQnSLmHFMsEf/JVjkm', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-05-01', 'Thành phố Hà Nội, Quận Hoàng Mai, Phường Hoàng Văn Thụ, asdfasdf', 0),
+(32, 'Trần Thẩm Khang', 'admin123', '$2y$10$whqz9WbpkYb79VkXw0r9S.m.E7t2amoXCCXhWkXx2tUPyk/2fGz2y', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-05-03', 'Tỉnh Tuyên Quang, Huyện Na Hang, Xã Thượng Giáp, sdafasdf', 0),
+(33, 'Trần Thẩm Khangasdf', 'wefwaf', '$2y$10$A7nA92T3MNVl78JJRHJSeOqdXhb3t0WKRSS1g3MxS0CYPsJsQY9.K', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-05-02', 'Tỉnh Quảng Ninh, Huyện Ba Chẽ, Xã Thanh Lâm, asdfdsag', 0),
+(34, 'Trần Thẩm Khang', 'dfa', '$2y$10$Ug7PkdDk3w1qdLHwiLeRg.388Wi5ks0LIkEF0lxJb2GHgbQyH626u', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-05-03', 'Tỉnh Quảng Ninh, Huyện Tiên Yên, Xã Đại Dực, asdfasdf', 0),
+(37, 'Trần Thẩm Khang', 'admin@gmail.comasdf', '$2y$10$vTTbIMMwle5ubALiVM/y8eo8I6WchxIz.D8JR8GQwlCoCC5HaFueS', 'thamkhang2003@gmail.com', 'Nam', '2147483647', '2023-05-18', 'sdafs', 0),
+(40, 'cdad', 'hautranad', '778899', 'tranhuuhau989@gmail.com', 'Nam', '0333333333', '2023-05-04', 'nguyeanc acdbj', 0),
+(41, 'baitap$', 'hautran', '778899', 'tranhuuhau98@gmail.com', 'Nam', '0333333333', '2023-05-03', 'dddd', 0),
+(42, 'áda', 'hautranads', '778899', 'tranhuasduhau98@gmail.com', 'Nam', '0333333333', '2023-05-02', 'adcasadd', 0);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -385,7 +388,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT cho bảng `user`
 --
 ALTER TABLE `user`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
